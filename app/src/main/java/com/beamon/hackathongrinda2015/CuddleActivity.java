1f17d151fca0f970f885e7f6c9b5444de32f5e62
tokko <andreg@kth.se>
2015-05-08 14:23:18 +0200

    fullscreen cuddle

--- app/src/main/java/com/beamon/hackathongrinda2015/CuddleActivity.java
+++ app/src/main/java/com/beamon/hackathongrinda2015/CuddleActivity.java
@@ -2,6 +2,8 @@ package com.beamon.hackathongrinda2015;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class CuddleActivity extends Activity {
 
@@ -9,5 +11,8 @@ public class CuddleActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getActionBar().hide();
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 }
