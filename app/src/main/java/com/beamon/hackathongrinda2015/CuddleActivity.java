43b09eb60ffd889da217aef6b5b04c4980d56f0e
tokko <andreg@kth.se>
2015-05-08 14:18:52 +0200

    hiding actionbar

--- app/src/main/java/com/beamon/hackathongrinda2015/CuddleActivity.java
+++ app/src/main/java/com/beamon/hackathongrinda2015/CuddleActivity.java
@@ -1,7 +1,13 @@
 package com.beamon.hackathongrinda2015;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class CuddleActivity extends Activity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getActionBar().hide();
+    }
 }
--- app/src/main/java/com/beamon/hackathongrinda2015/MainActivity.java
+++ app/src/main/java/com/beamon/hackathongrinda2015/MainActivity.java
@@ -57,6 +57,7 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        getActionBar().hide();
         setContentView(R.layout.activity_main);
       //  mDisplay = (TextView) findViewById(R.id.display);
 
