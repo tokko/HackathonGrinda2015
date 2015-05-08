d861f66dfe857c98f2ce8d7a345039f345c193e9
tokko <andreg@kth.se>
2015-05-08 11:06:27 +0200

    preparation for push

---
+++ app/src/main/java/com/beamon/hackathongrinda2015/PushReceiver.java
@@ -0,0 +1,15 @@
+package com.beamon.hackathongrinda2015;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by andre_000 on 2015-05-08.
+ */
+public class PushReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
